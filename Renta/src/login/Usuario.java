0c0a2bd49d3309bfd5700e90632fee3c673918a0
Brayan Esteven Bernal Martínez <100706145+LuffyBB1@users.noreply.github.com>
2022-08-31 10:42:38 -0500

    Update Usuario.java

--- Renta/src/login/Usuario.java
+++ Renta/src/login/Usuario.java
@@ -1,34 +1,41 @@
-package login;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication14;
+
 /**
  *
+ * @author Luffy
  */
 public class Usuario {
-    private int IDUsuario;
+    private String IDUsuario;
     private String Usuario;
     private String Nombre;
     private String Identificacion;
     private String tipo_user;
+    private String pssw;
     
     private static Usuario instance;
-   
-    
-    public Usuario(int idusuario, String usuario, String nombre, String identificacion, String tipo_user ){
-        this.IDUsuario = idusuario;
-        this.Usuario = usuario;
-        this.Nombre = nombre;
-        this.Identificacion = identificacion;
+
+    public Usuario(String IDUsuario, String Usuario, String Nombre, String Identificacion, String tipo_user, String pssw) {
+        this.IDUsuario = IDUsuario;
+        this.Usuario = Usuario;
+        this.Nombre = Nombre;
+        this.Identificacion = Identificacion;
         this.tipo_user = tipo_user;
+        this.pssw = pssw;
     }
-    
+       
     private Usuario(){
         
     }
 
-    public int getIDUsuario() {
+    public String getIDUsuario() {
         return IDUsuario;
     }
 
-    public void setIDUsuario(int IDUsuario) {
+    public void setIDUsuario(String IDUsuario) {
         this.IDUsuario = IDUsuario;
     }
 
@@ -57,6 +64,16 @@ public class Usuario {
     public void setIdTipoUsuario(String IdTipoUsuario) {
         this.tipo_user = IdTipoUsuario;
     }
+
+    public String getPssw() {
+        return pssw;
+    }
+
+    public void setPssw(String pssw) {
+        this.pssw = pssw;
+    }
+   
+    
     public static Usuario getInstance() {
         if (instance == null)
             instance = new Usuario();
