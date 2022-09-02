870d2e03ef9d3b0f6fec383334d80c8f14ba62b1
LuffyBB <101308335+LuffyBB@users.noreply.github.com>
2022-09-02 16:53:36 -0500

    Update Login.java

--- Renta/src/login/Login.java
+++ Renta/src/login/Login.java
@@ -8,10 +8,10 @@ public class Login {
     
     private ArrayList<Usuario> users =new ArrayList<>();
      public Login() {
-        users.add(new Usuario("1","hugo_admin","Hugo Calderon","100345","admin", "hugo123"));
-        users.add(new Usuario("2","juanpa12","Juan Pablo Rodriguez","11001346","cliente", "jun12"));
-        users.add(new Usuario("3","camilo15","Camilo Cardenas","1004475","cliente", "1004475"));
-        users.add(new Usuario("4","alejandroking","Alejaandro Villamil","52600454","cliente", "tatoabogado"));
+        users.add(new Usuario("1","hugo_admin","Hugo Calderon","100345","admin", "hugo123",true));
+        users.add(new Usuario("2","juanpa12","Juan Pablo Rodriguez","11001346","cliente", "jun12",false));
+        users.add(new Usuario("3","camilo15","Camilo Cardenas","1004475","cliente", "1004475",true));
+        users.add(new Usuario("4","alejandroking","Alejaandro Villamil","52600454","cliente", "tatoabogado",false));
     }
      
     public Usuario validar (String pssw, String user){
@@ -32,6 +32,7 @@ public class Login {
         return false;
         }
     }
+   
    
  
 
