b318d693272927640c440050a4c55ceb16d78f83
LuffyBB <101308335+LuffyBB@users.noreply.github.com>
2022-09-02 16:30:53 -0500

    Update Login.java

--- Renta/src/login/Login.java
+++ Renta/src/login/Login.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
 public class Login {
     
     private ArrayList<Usuario> users =new ArrayList<>();
-    public Login() {
+     public Login() {
         users.add(new Usuario("1","hugo_admin","Hugo Calderon","100345","admin", "hugo123"));
         users.add(new Usuario("2","juanpa12","Juan Pablo Rodriguez","11001346","cliente", "jun12"));
         users.add(new Usuario("3","camilo15","Camilo Cardenas","1004475","cliente", "1004475"));
         users.add(new Usuario("4","alejandroking","Alejaandro Villamil","52600454","cliente", "tatoabogado"));
     }
-   
+     
     public Usuario validar (String pssw, String user){
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getUsuario().equals(user) && users.get(i).getPssw().equals(pssw)) {
@@ -29,32 +29,9 @@ public class Login {
         if (user.getIdTipoUsuario()=="admon"){
             return true;
         };
-        
         return false;
         }
-        
-        
     }
-        
-    
-    /*public Usuario validar (String pssw, String user){
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getUsuario()==user && users.get(i).getPssw()==pssw) {
-                return users.get(i);
-            }
-     
-        }
-        return null;
-    }
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Login l = new Login();
-        String pss="hugo123", user="hugo_admin";
-        Usuario r=l.validar(pss, user);
-        System.out.println("R: "+r.getNombreApellidos());
-        System.out.println("Tipo Usuario: "+r.getIdTipoUsuario());
-        
-    }*/
+   
  
 
