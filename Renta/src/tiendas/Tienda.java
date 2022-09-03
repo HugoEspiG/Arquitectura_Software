03deadb378332d953bcdb2f790b90f7e22ba824a
Sala Yellow <yellow@academia.loc>
2022-09-02 21:48:57 -0500

    Se arregla el paquete tiendas

--- Renta/src/tiendas/Tienda.java
+++ Renta/src/tiendas/Tienda.java
@@ -1,41 +1,42 @@
-package tiendas;
+package Renta.src.tiendas;
 public class Tienda {
 	
 	
 	int caja;
-	producto pro1;
-	producto pro2;
-	producto pro3;
-	producto pro4;
+	Producto pro1;
+	Producto pro2;
+	Producto pro3;
+	Producto pro4;
 	
-	public void hacerPedido() {
-		producto pro1 = new producto("Jugo hit", "3450", 9.2);
-		System.out.println(pro1.nombre+": "+pro1.precio+": "+pro1.id);
-		
+	public void hacerpedido(String nombre, String id, Double precio){
+		Producto pro1 = new Producto(nombre, id, precio);		
+		Producto pro2 = new Producto(nombre, id, precio);		
+		Producto pro3 = new Producto(nombre, id, precio);		
+		Producto pro4 = new Producto(nombre, id, precio);		
 	}
 	
-	public producto getPro1() {
+	public Producto getPro1() {
 		return pro1;
 	}
-	public void setP1(producto pro1) {
+	public void setP1(Producto pro1) {
 		this.pro1 = pro1;
 	}
-	public producto getPro2() {
+	public Producto getPro2() {
 		return pro2;
 	}
-	public void setPro2(producto pro2) {
+	public void setPro2(Producto pro2) {
 		this.pro2 = pro2;
 	}
-	public producto getPro3() {
+	public Producto getPro3() {
 		return pro3;
 	}
-	public void setPro3(producto pro3) {
+	public void setPro3(Producto pro3) {
 		this.pro3 = pro3;
 	}
-	public producto getPro4() {
+	public Producto getPro4() {
 		return pro4;
 	}
-	public void setP4(producto pro4) {
+	public void setP4(Producto pro4) {
 		this.pro4 = pro4;
 	}
 
