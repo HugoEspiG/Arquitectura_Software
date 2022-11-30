9ffd6f835b476424ea8ba2e12a31a4aa4d3bd29d
Alejo Suaterna <101911053+AlejoSuaterna@users.noreply.github.com>
2022-11-30 00:31:07 -0500

    Delete backend to push another one manually

--- BackEnd/src/main/java/com/example/websocketi/repository/crudRepository/MonaCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.websocketi.repository.crudRepository;
-
-import com.example.websocketi.model.User;
-import com.example.websocketi.model.Mona;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MonaCrudRepository extends CrudRepository<Mona,Integer> {
-
-}
